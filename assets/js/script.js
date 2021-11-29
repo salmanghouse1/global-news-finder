3474613f537c54aaa81fb7e29f2f4e5a49b91706
Ethan <90936828+EthanC29@users.noreply.github.com>
2021-11-29 01:35:45 -0500

    created function to create object with required info from the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,42 @@
 //Testing api calls
 
-everything_api = "https://saurav.tech/NewsAPI/everything/bbc-news.json"
+let country = {};
+function getRestAPI(countryName) {
 
-apiUrl = 'https://saurav.tech/NewsAPI/top-headlines/category/health/in.json';
+    rest_api = "https://restcountries.com/v3.1/name/" + countryName;
 
+    apiUrl = 'https://saurav.tech/NewsAPI/top-headlines/category/health/in.json';
 
-fetch(everything_api).then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+    
+    fetch(rest_api)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            var currencyObject = data[0].currencies;
+            var currency = Object.keys(currencyObject);
+
+            var languageObject = data[0].languages;
+            var language = Object.keys(languageObject);
+
+            country = {
+                "cname": data[0].name.official,
+                "currency": data[0].currencies[currency[0]].name + " (" + data[0].currencies[currency[0]].symbol + currency[0] + ")",
+                "primaryLanguage": data[0].languages[language[0]],
+                "secondaryLanguage": data[0].languages[language[1]],
+                "capital": data[0].capital[0],
+                "population": data[0].population,
+                "flag": data[0].flags.png,
+                "coatofarms": data[0].coatOfArms.png
+            }
+
+            console.log(country)
+
+        })
+        .catch(err => console.error(err));
+
+    
+};
+
+getRestAPI("Canada");
