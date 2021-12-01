bce072bfa54913cc33396949f5575b44fbfdc8a7
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-30 22:47:53 -0500

    mapping is done, generating new app on fetch-openlayers api

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,6 +54,8 @@ function countryNews(country) {
                 `;
 
                     //* Append to div
+
+                    // added slide down animation
                     $('article').slideDown(700, function() {
                         newsSection.innerHTML = output;
 
@@ -106,6 +108,29 @@ function getRestAPI(countryName) {
             var languageObject = data[0].languages;
             var language = Object.keys(languageObject);
 
+            // Start open layers code
+
+
+            document.getElementById('map').innerHTML = "";
+
+            var map = new ol.Map({
+                target: 'map',
+                layers: [
+                    new ol.layer.Tile({
+                        source: new ol.source.OSM()
+                    })
+                ],
+                view: new ol.View({
+                    center: ol.proj.fromLonLat([data[0].capitalInfo.latlng[1], data[0].capitalInfo.latlng[0]]),
+                    zoom: 4
+                })
+            });
+
+            //End open layers code 
+
+
+
+
             maincountry = {
                 commonName: data[0].name.common,
                 currency: data[0].currencies[currency[0]].name +
@@ -129,74 +154,81 @@ function getRestAPI(countryName) {
 
 //--------------------- Shayne's Section-------------------
 
-function initAutocomplete() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -33.8688, lng: 151.2195 },
-        zoom: 13,
-        mapTypeId: "roadmap",
-    });
-    // Create the search box and link it to the UI element.
-    const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input || select.value);
+// Start open layers code
 
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-        input || select.value
-    );
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener("bounds_changed", () => {
-        searchBox.setBounds(map.getBounds());
-    });
 
-    let markers = [];
 
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    searchBox.addListener("places_changed", () => {
-        const places = searchBox.getPlaces();
+//End open layers code 
 
-        if (places.length == 0) {
-            return;
-        }
 
-        // Clear out the old markers.
-        markers.forEach((marker) => {
-            marker.setMap(null);
-        });
-        markers = [];
+// function initAutocomplete() {
+//     const map = new google.maps.Map(document.getElementById("map"), {
+//         center: { lat: -33.8688, lng: 151.2195 },
+//         zoom: 13,
+//         mapTypeId: "roadmap",
+//     });
+//     // Create the search box and link it to the UI element.
+//     const input = document.getElementById("pac-input");
+//     const searchBox = new google.maps.places.SearchBox(input || select.value);
 
-        // For each place, get the icon, name and location.
-        const bounds = new google.maps.LatLngBounds();
+//     map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+//         input || select.value
+//     );
+//     // Bias the SearchBox results towards current map's viewport.
+//     map.addListener("bounds_changed", () => {
+//         searchBox.setBounds(map.getBounds());
+//     });
 
-        places.forEach((place) => {
-            if (!place.geometry || !place.geometry.location) {
-                console.log("Returned place contains no geometry");
-                return;
-            }
+//     let markers = [];
 
-            const icon = {
-                url: place.icon,
-                size: new google.maps.Size(71, 71),
-                origin: new google.maps.Point(0, 0),
-                anchor: new google.maps.Point(17, 34),
-                scaledSize: new google.maps.Size(25, 25),
-            };
+//     // Listen for the event fired when the user selects a prediction and retrieve
+//     // more details for that place.
+//     searchBox.addListener("places_changed", () => {
+//         const places = searchBox.getPlaces();
 
-            // Create a marker for each place.
-            markers.push(
-                new google.maps.Marker({
-                    map,
-                    icon,
-                    title: place.name,
-                    position: place.geometry.location,
-                })
-            );
-            if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-            } else {
-                bounds.extend(place.geometry.location);
-            }
-        });
-        map.fitBounds(bounds);
-    });
-}
+//         if (places.length == 0) {
+//             return;
+//         }
+
+//         // Clear out the old markers.
+//         markers.forEach((marker) => {
+//             marker.setMap(null);
+//         });
+//         markers = [];
+
+//         // For each place, get the icon, name and location.
+//         const bounds = new google.maps.LatLngBounds();
+
+//         places.forEach((place) => {
+//             if (!place.geometry || !place.geometry.location) {
+//                 console.log("Returned place contains no geometry");
+//                 return;
+//             }
+
+//             const icon = {
+//                 url: place.icon,
+//                 size: new google.maps.Size(71, 71),
+//                 origin: new google.maps.Point(0, 0),
+//                 anchor: new google.maps.Point(17, 34),
+//                 scaledSize: new google.maps.Size(25, 25),
+//             };
+
+//             // Create a marker for each place.
+//             markers.push(
+//                 new google.maps.Marker({
+//                     map,
+//                     icon,
+//                     title: place.name,
+//                     position: place.geometry.location,
+//                 })
+//             );
+//             if (place.geometry.viewport) {
+//                 // Only geocodes have viewport.
+//                 bounds.union(place.geometry.viewport);
+//             } else {
+//                 bounds.extend(place.geometry.location);
+//             }
+//         });
+//         map.fitBounds(bounds);
+//     });
+// }
